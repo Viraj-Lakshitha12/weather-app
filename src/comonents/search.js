f490248a38331115b459561048a2b51e177456f0
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-02-12 09:30:40 +0530

    config tailwind and add search text

--- src/comonents/search.js
+++ src/comonents/search.js
@@ -1,7 +1,22 @@
-export const Search = () => {
-    return(
-        <div>
-            Search
+import {AsyncPaginate} from "react-select-async-paginate"
+import {useState} from "react";
+
+export const Search = ({onSearchChange}) => {
+    const [search, setSearch] = useState(null);
+
+    function handleOnChange(searchData) {
+        setSearch(searchData);
+        onSearchChange(searchData)
+    }
+
+    return (
+        <div class={'mt-1'}>
+            <AsyncPaginate
+                placeholder={'search for city'}
+                value={search}
+                onChange={handleOnChange}
+                debounceTimeout={600}
+            ></AsyncPaginate>
         </div>
     )
 }
